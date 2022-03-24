5bc27999392f8377cef9f61fafb9062270ea9b79
Elshan Alkhabaz <elshanx@icloud.com>
2022-03-24 22:47:25 +0400

    add new test cases and node mods to gitignore

--- circular-buffer.spec.js
+++ circular-buffer.spec.js
@@ -1,7 +1,4 @@
-import CircularBuffer, {
-  BufferFullError,
-  BufferEmptyError,
-} from './circular-buffer';
+import CircularBuffer, { BufferFullError, BufferEmptyError } from './circular-buffer';
 
 describe('CircularBuffer', () => {
   test('reading empty buffer should fail', () => {
@@ -13,5 +10,32 @@ describe('CircularBuffer', () => {
     const buffer = new CircularBuffer(1);
     buffer.write('1');
     expect(buffer.read()).toBe('1');
+  });
+
+  test('forcewriting to buffer should overwrite old value', () => {
+    const buffer = new CircularBuffer(1);
+    buffer.write('1');
+    buffer.forceWrite('2');
+    expect(buffer.read()).toBe('2');
+  });
+
+  test('writing to a full buffer throws BufferFullError', () => {
+    const buffer = new CircularBuffer(1);
+    buffer.write('1');
+    expect(() => buffer.write('2')).toThrow(BufferFullError);
+  });
+
+  test('reading from an empty buffer throws BufferEmptyError', () => {
+    const buffer = new CircularBuffer(1);
+    expect(() => buffer.read()).toThrow(BufferEmptyError);
+  });
+
+  test('clearing a buffer resets the buffer', () => {
+    const buffer = new CircularBuffer(5);
+    for (let i = 0; i < buffer.capacity; i++) {
+      buffer.write(i);
+    }
+    buffer.clear();
+    expect(buffer.isEmpty()).toBe(true);
   });
 });
